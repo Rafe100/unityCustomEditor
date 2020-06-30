ef34de74914aec8c20ea6fabcefc060d7257f014
rafe <936523016@qq.com>
2020-06-30 20:55:16 +0800

    pInvoke

--- customEditor/Assets/Editor/CustomTransformEditor.cs
+++ customEditor/Assets/Editor/CustomTransformEditor.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Runtime.InteropServices;
+using System;
 
 [CustomEditor(typeof(CustomTransform))]
 public class CustomTransformEditor : Editor
 {
+    [DllImport("user32.dll")]
+    public static extern int MessageBox(IntPtr handle, String message, String title, int type);
+    static bool m_showDetails;
     public override void OnInspectorGUI()
     {
         CustomTransform customTarget = (CustomTransform)target;
@@ -46,5 +51,24 @@ public class CustomTransformEditor : Editor
             EditorGUILayout.LabelField("Target: " + customTarget.gameObject.name);
             EditorGUILayout.EndVertical();
         }
+
+        {
+            GUI.backgroundColor = Color.cyan;
+            if (GUILayout.Button("SelectObject"))
+            {
+                Selection.activeGameObject = GameObject.Find("SelectObject");
+            }
+            if (GUILayout.Button("MessageBox_0"))
+            {
+                int result = MessageBox(IntPtr.Zero, "this is a message from ET", "Oh!!!!!!!!!!!!!!", 0);
+                Debug.Log("messageBox result " + result);
+            }
+            if (GUILayout.Button("MessageBox_1"))
+            {
+                int result = MessageBox(IntPtr.Zero, "this is a message from ET", "Oh!!!!!!!!!!!!!!", 1);
+                Debug.Log("messageBox result " + result);
+            }
+        }
+
     }
 }
